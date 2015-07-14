eee17d1d9a80cc4e16053201bd0e90bda42a8d28
Cris Stanza <crisstanza@work>
2015-07-14 19:24:32 -0300

    Implementation: setRandomValues

--- src/utils/IntrospectionUtils.java
+++ src/utils/IntrospectionUtils.java
@@ -1,6 +1,11 @@
 package utils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Cris Stanza, 10-Jul-2015.
@@ -23,6 +28,46 @@ public class IntrospectionUtils {
 		} catch (Exception exc) {
 			throw new RuntimeException(exc);
 		}
+	}
+
+	public static Object setRandomValues(Object obj) {
+		try {
+			Class<?> clazz = obj.getClass();
+			for (Field field : clazz.getDeclaredFields()) {
+				field.setAccessible(true);
+				if (!Modifier.isFinal(field.getModifiers())) {
+					field.set(obj, randomValue(field));
+				}
+				field.setAccessible(false);
+			}
+			return obj;
+		} catch (Exception exc) {
+			throw new RuntimeException(exc);
+		}
+	}
+
+	private static Object randomValue(Field field) throws Exception {
+		Class<?> type = field.getType();
+		if (String.class.getName().equals(type.getName())) {
+			return RandomUtils.randomCaptalizedString();
+		} else if (Integer.class.getName().equals(type.getName())) {
+			return RandomUtils.randomInt();
+		} else if (Double.class.getName().equals(type.getName())) {
+			return RandomUtils.randomDouble();
+		} else if (List.class.getName().equals(type.getName())) {
+			return randomValue(new ArrayList<>(), field);
+		} else {
+			return null;
+		}
+	}
+
+	private static Object randomValue(ArrayList<Object> list, Field field) throws Exception {
+		ParameterizedType genericType = (ParameterizedType) field.getGenericType();
+		Type[] genericTypeArguments = genericType.getActualTypeArguments();
+		Class<?> genericClass = (Class<?>) genericTypeArguments[0];
+		Object newObject = genericClass.newInstance();
+		list.add(setRandomValues(newObject));
+		return list;
 	}
 
 }
